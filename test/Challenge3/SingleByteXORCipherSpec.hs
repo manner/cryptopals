f388d6c476e4b0a87b447a6bbddad22d5bb2f19e
Moritz Manner <moritz.manner@gmail.com>
2020-11-24 17:50:57 +0100

    Add spec for Challenge 3

---
+++ test/Challenge3/SingleByteXORCipherSpec.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Challenge3.SingleByteXORCipherSpec (spec) where
+
+import Test.Hspec
+import Challenge3.SingleByteXORCipher
+
+spec :: Spec
+spec = do
+  describe "SingleByteXORCipher" $ do
+    it "find correct text from cipher text which has been xored with single byte" $
+      decrypt "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736" `shouldBe` ('X', "Cooking MC's like a pound of bacon")
+
+    
+

bd148f2340f3f8c8c3501710db6544a45f98f51b
Moritz Manner <moritz.manner@gmail.com>
2020-11-25 19:05:08 +0100

    Add spec for Challenge 6

--- test/Challenge6/DecryptRepeatingXORSpec.hs
+++ test/Challenge6/DecryptRepeatingXORSpec.hs
@@ -2,10 +2,16 @@
 module Challenge6.DecryptRepeatingXORSpec (spec) where
 
 import Test.Hspec
+import qualified Data.ByteString.Lazy as BL
+import Challenge1.ConvertHexToBase64 (base64ToBytes)
 import Challenge6.DecryptRepeatingXOR
 
 spec :: Spec
 spec = do
   describe "Decrypt cipher" $ do
-    it "correctly calculates bit hamming distance" $
+    it "correctly calculates bitstac hamming distance" $
       hamming "this is a test" "wokka wokka!!!" `shouldBe` 37
+    it "finds key for repeating cipher" $ do 
+      contents <- BL.readFile filename
+      let cipher = base64ToBytes contents
+      findKeys cipher `shouldBe` ["Terminator X: Bring the noise"]
